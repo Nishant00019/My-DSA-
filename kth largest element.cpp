548d382cc4f4a7d33a1ca27637f0b64506528de8
Nishant Kaushal <90994579+Nishant00019@users.noreply.github.com>
2023-01-29 22:11:04 +0530

    Create kth largest element.cpp

---
+++ kth largest element.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++)
+  {
+    cin>>a[i];
+  }
+  int k;
+  cin>>k;
+  priority_queue<int>p;
+  for(int i=0; i<=n; i++)
+  {
+    p.push(a[i]);
+  }   
+  while(k--)
+  {
+    p.pop();
+  }
+  cout<<p.top();
+}
+
+//Time complexity : O(N)
